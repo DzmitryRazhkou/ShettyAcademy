1ea4b9b397ef66494c40513a81a208c250cdb61f
DzmitryRazhkou <dzmitryrazhkou@gmail.com>
2022-06-18 23:20:39 -0700

    OrderHistoryPage page is done

--- src/main/java/com/mystore/qa/pages/OrderHistoryPage.java
+++ src/main/java/com/mystore/qa/pages/OrderHistoryPage.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 public class OrderHistoryPage {
 
@@ -46,4 +47,77 @@ public class OrderHistoryPage {
         return driver.getTitle();
     }
 
+//    ORDER REFERENCE:
+
+    public boolean getOrderReference(String orderReferenceName){
+        By orderReferenceLocator = By.cssSelector(".color-myaccount");
+        wait.until(ExpectedConditions.presenceOfElementLocated(orderReferenceLocator));
+
+        List<WebElement> orderReferenceList = driver.findElements(orderReferenceLocator);
+        for (WebElement s: orderReferenceList){
+            if (s.getText().trim().contains(orderReferenceName)){
+                System.out.println("Order reference list contains: " +orderReferenceName);
+                return true;
+            }
+        }
+        return false;
+    }
+
+//    DATA:
+
+    public boolean getDate(String dateValue){
+        By dateLocator = By.xpath("//*[@class='history_date bold']");
+        wait.until(ExpectedConditions.presenceOfElementLocated(dateLocator));
+
+        List<WebElement> dateList = driver.findElements(dateLocator);
+        for (WebElement s: dateList){
+            if (s.getText().trim().contains(dateValue)){
+                System.out.println("Date list contains: " +dateValue);
+                return true;
+            }
+        }
+        return false;
+    }
+
+//    TOTAL PRICE:
+
+    public boolean getTotalPrice(String totalPrice){
+        By totalPriceLocator = By.cssSelector(".history_price");
+        wait.until(ExpectedConditions.presenceOfElementLocated(totalPriceLocator));
+
+        List<WebElement> dateList = driver.findElements(totalPriceLocator);
+        for (WebElement s: dateList){
+            if (s.getText().trim().contains(totalPrice)){
+                System.out.println("Date list contains: " +totalPrice);
+                return true;
+            }
+        }
+        return false;
+    }
+
+//    Back to your account:
+
+    private WebElement getBackToYourAccount() {
+        By getBackToYourAccountLocator = By.xpath("(//*[@class='btn btn-default button button-small'])[10]");
+        wait.until(ExpectedConditions.presenceOfElementLocated(getBackToYourAccountLocator));
+        return driver.findElement(getBackToYourAccountLocator);
+    }
+
+    public MyAccountPage doClickBackToToYourAccount() {
+        getBackToYourAccount().click();
+        return new MyAccountPage(driver);
+    }
+
+//    Home:
+
+    private WebElement getHome() {
+        By getHomeLocator = By.xpath("(//*[@class='btn btn-default button button-small'])[11]");
+        wait.until(ExpectedConditions.presenceOfElementLocated(getHomeLocator));
+        return driver.findElement(getHomeLocator);
+    }
+
+    public MyStorePage doClickHome() {
+        getHome().click();
+        return new MyStorePage(driver);
+    }
 }
--- src/test/java/com/mystore/qa/testcases/OrderHistoryPageTest.java
+++ src/test/java/com/mystore/qa/testcases/OrderHistoryPageTest.java
@@ -61,4 +61,57 @@ public class OrderHistoryPageTest {
         Assert.assertEquals(expOrderHistoryPageTitle, actOrderHistoryPageTitle);
     }
 
+    @Test
+    public void getOrderReferenceTest() {
+        myStorePage = new MyStorePage(driver);
+        loginPage = myStorePage.clickSignIn();
+        myAccountPage = loginPage.doLogin(prop.getProperty("email"), prop.getProperty("password"));
+        orderHistoryPage = myAccountPage.clickOnOrderHistory();
+        String orderReference = prop.getProperty("orderReference");
+        Assert.assertTrue(orderHistoryPage.getOrderReference(orderReference));
+    }
+
+    @Test
+    public void getDateTest() {
+        myStorePage = new MyStorePage(driver);
+        loginPage = myStorePage.clickSignIn();
+        myAccountPage = loginPage.doLogin(prop.getProperty("email"), prop.getProperty("password"));
+        orderHistoryPage = myAccountPage.clickOnOrderHistory();
+        String date = prop.getProperty("date");
+        Assert.assertTrue(orderHistoryPage.getDate(date));
+    }
+
+    @Test
+    public void getTotalPriceTest() {
+        myStorePage = new MyStorePage(driver);
+        loginPage = myStorePage.clickSignIn();
+        myAccountPage = loginPage.doLogin(prop.getProperty("email"), prop.getProperty("password"));
+        orderHistoryPage = myAccountPage.clickOnOrderHistory();
+        String totalPrice = prop.getProperty("totalPrice");
+        Assert.assertTrue(orderHistoryPage.getTotalPrice(totalPrice));
+    }
+
+    @Test
+    public void doClickOnBackToYourAccountTest() {
+        myStorePage = new MyStorePage(driver);
+        loginPage = myStorePage.clickSignIn();
+        myAccountPage = loginPage.doLogin(prop.getProperty("email"), prop.getProperty("password"));
+        orderHistoryPage = myAccountPage.clickOnOrderHistory();
+        myAccountPage = orderHistoryPage.doClickBackToToYourAccount();
+        String actMyAccountPageTitle = myAccountPage.getMyAccountPageTitle();
+        String expMyAccountPageTitle = prop.getProperty("myAccountPageTitle");
+        Assert.assertEquals(expMyAccountPageTitle, actMyAccountPageTitle);
+    }
+
+    @Test
+    public void doClickOnHomeTest() {
+        myStorePage = new MyStorePage(driver);
+        loginPage = myStorePage.clickSignIn();
+        myAccountPage = loginPage.doLogin(prop.getProperty("email"), prop.getProperty("password"));
+        orderHistoryPage = myAccountPage.clickOnOrderHistory();
+        myStorePage = orderHistoryPage.doClickHome();
+        String actMyStorePageTitle = myStorePage.getMyStorePageTitle();
+        String expMyStorePageTitle = prop.getProperty("myStorePageTitle");
+        Assert.assertEquals(expMyStorePageTitle, actMyStorePageTitle);
+    }
 }
